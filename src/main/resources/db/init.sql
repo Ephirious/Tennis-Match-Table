d1bf69f575f3a47088829f66db617aebfb20ff12
Danila <Ephirious@gmail.com>
2026-07-22 18:56:54 +0300

    chore: изменено название полей в таблице matches

--- src/main/resources/db/init.sql
+++ src/main/resources/db/init.sql
@@ -7,13 +7,13 @@ create table players (
 
 create table matches (
     id uuid primary key default uuidv7(),
-    first_player uuid not null,
-    second_player uuid not null,
-    winner uuid,
+    first_player_id uuid not null,
+    second_player_id uuid not null,
+    winner_id uuid not null,
 
-    constraint fk_first_player foreign key (first_player) references players(id),
-    constraint fk_second_player foreign key (second_player) references players(id),
-    constraint fk_winner foreign key (winner) references players(id),
-    constraint check_different_players check (first_player <> second_player),
-    constraint check_winner check (winner is null or winner = first_player or winner = second_player)
+    constraint fk_first_player foreign key (first_player_id) references players(id),
+    constraint fk_second_player foreign key (second_player_id) references players(id),
+    constraint fk_winner foreign key (winner_id) references players(id),
+    constraint check_different_players check (first_player_id <> second_player_id),
+    constraint check_winner check (winner_id is null or winner_id = first_player_id or winner_id = second_player_id)
 );
