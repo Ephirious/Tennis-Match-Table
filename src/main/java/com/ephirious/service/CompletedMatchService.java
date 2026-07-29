f3348fd0811c3af77a5407c4f0cf51efa779414d
Danila <Ephirious@gmail.com>
2026-07-29 20:39:45 +0300

    refactor: переписаны сервисы, которые теперь управляют тразакциями

--- src/main/java/com/ephirious/service/CompletedMatchService.java
+++ src/main/java/com/ephirious/service/CompletedMatchService.java
@@ -2,17 +2,23 @@ package com.ephirious.service;
 
 import com.ephirious.model.aggregate.Match;
 import com.ephirious.repository.CompletedMatchRepository;
+import com.ephirious.transaction.TransactionManager;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CompletedMatchService {
     private final CompletedMatchRepository repository;
+    private final TransactionManager transactionManager;
 
-    public CompletedMatchService(CompletedMatchRepository postgresCompletedMatchRepository) {
+    public CompletedMatchService(
+            CompletedMatchRepository postgresCompletedMatchRepository,
+            TransactionManager transactionManager
+    ) {
         this.repository = postgresCompletedMatchRepository;
+        this.transactionManager = transactionManager;
     }
 
     public void save(Match match) {
-        repository.add(match);
+        transactionManager.executeInTransaction(() -> repository.add(match));
     }
 }
--- src/main/java/com/ephirious/service/PlayerService.java
+++ src/main/java/com/ephirious/service/PlayerService.java
@@ -3,24 +3,28 @@ package com.ephirious.service;
 import com.ephirious.model.entity.Player;
 import com.ephirious.model.value.PlayerName;
 import com.ephirious.repository.PlayerRepository;
+import com.ephirious.transaction.TransactionManager;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PlayerService {
     private final PlayerRepository repository;
+    private final TransactionManager transactionManager;
 
-    public PlayerService(PlayerRepository postgresPlayerRepository) {
+    public PlayerService(PlayerRepository postgresPlayerRepository, TransactionManager transactionManager) {
         this.repository = postgresPlayerRepository;
+        this.transactionManager = transactionManager;
     }
 
     public Player getOrCreatePlayer(PlayerName name) {
         Player newPlayer = new Player(name);
-        return repository.addIfAbsent(newPlayer);
+        return transactionManager.executeInTransactionReturned(() -> repository.addIfAbsent(newPlayer));
     }
 
     public Player findByName(PlayerName name) {
-        return repository.findByName(name)
-                .orElseThrow(() -> new IllegalStateException("Unknown player name '%s'".formatted(name.value()))
+        return transactionManager.executeInTransactionReturned(() -> repository.findByName(name))
+                .orElseThrow(
+                        () -> new IllegalStateException("Unknown player name '%s'".formatted(name.value()))
                 );
     }
 }
